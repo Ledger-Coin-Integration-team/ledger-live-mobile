5afedebc7280de3fef4634009f391ec4fc48f1e8
Rémi Jarasson (Ext) <remi.jarasson-ext@ledger.fr>
2021-01-13 13:37:37 +0100

    feat(Polkadot): add fees + info in device confirm

--- src/families/polkadot/TransactionConfirmFields.js
+++ src/families/polkadot/TransactionConfirmFields.js
@@ -1,6 +1,8 @@
 // @flow
+import invariant from "invariant";
 import React, { useMemo, useCallback } from "react";
-import { Linking } from "react-native";
+import { StyleSheet, Linking, View } from "react-native";
+import { useTranslation } from "react-i18next";
 
 import type { Account } from "@ledgerhq/live-common/lib/types";
 import type { Transaction } from "@ledgerhq/live-common/lib/families/polkadot/types";
@@ -10,9 +12,13 @@ import {
   getAddressExplorer,
 } from "@ledgerhq/live-common/lib/explorers";
 
-import { HeaderRow } from "../../components/ValidateOnDeviceDataRow";
+import LText from "../../components/LText";
+import { DataRow, HeaderRow } from "../../components/ValidateOnDeviceDataRow";
+import colors from "../../colors";
+import InfoIcon from "../../icons/Info";
 
 import NominationInfo from "./components/NominationInfo";
+import PolkadotFeeRow from "./SendRowsFee";
 
 type FieldProps = {
   account: Account,
@@ -64,10 +70,76 @@ function PolkadotValidatorsField({ account, transaction, field }: FieldProps) {
   );
 }
 
+const Info = ({ transaction }: FieldProps) => {
+  invariant(transaction.family === "polkadot", "polkadot transaction");
+
+  const { t } = useTranslation();
+
+  switch (transaction.mode) {
+    case "bond":
+    case "unbond":
+    case "rebond":
+      return (
+        <DataRow>
+          <InfoIcon size={22} color={colors.live} />
+          <LText
+            semiBold
+            style={[styles.text, styles.infoText]}
+            numberOfLines={3}
+          >
+            {t(`polkadot.${transaction.mode}.steps.confirm.info`)}
+          </LText>
+        </DataRow>
+      );
+    default:
+      return null;
+  }
+};
+
+const EstimatedFees = ({ account, transaction }: FieldProps) => {
+  return (
+    <View style={styles.feesWrapper}>
+      <PolkadotFeeRow account={account} transaction={transaction} />
+    </View>
+  );
+};
+
+const Warning = (props: FieldProps) => {
+  return (
+    <>
+      <EstimatedFees {...props} />
+      <Info {...props} />
+    </>
+  );
+};
+
 const fieldComponents = {
   "polkadot.validators": PolkadotValidatorsField,
 };
 
+const styles = StyleSheet.create({
+  text: {
+    color: colors.darkBlue,
+    textAlign: "right",
+    flex: 1,
+  },
+  infoText: {
+    color: colors.live,
+    textAlign: "left",
+    marginLeft: 8,
+  },
+  feesWrapper: {
+    borderRadius: 4,
+    marginVertical: 1,
+    paddingHorizontal: 12,
+    borderTopWidth: 1,
+    borderStyle: "solid",
+    borderColor: colors.grey,
+    backgroundColor: colors.lightGrey,
+  },
+});
+
 export default {
   fieldComponents,
+  warning: Warning,
 };
